46cf4c99e414d9d5db43c71320d61d55556fb0a4
Aldrin <cleetusaldrin30@gmail.com>
2022-10-22 21:18:37 +0530

    file verification

--- main.js
+++ main.js
@@ -2,13 +2,33 @@ const shareArea = document.querySelector('.share')
 const input = shareArea.querySelector('input')
 const button = shareArea.querySelector('button')
 
+let uploadedFile;
+
+
+const acceptedFileFormat= ['application/pdf',
+'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+
+const isAccepted = ()=>{
+    const fileType = uploadedFile.type
+
+    if( acceptedFileFormat.includes(fileType)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
 button.onclick = ()=>{
     input.click()
 }
 
-// input.addEventListener('change',()=>{
-//     console.log('huh')
-// })
+input.addEventListener('change',()=>{
+    uploadedFile = this.files[0]
+
+    console.log(uploadedFile)
+})
 
 shareArea.addEventListener('dragover', e=>{
     e.preventDefault();
@@ -24,5 +44,13 @@ shareArea.addEventListener('dragleave', e=>{
 
 shareArea.addEventListener('drop', e =>{
     e.preventDefault();
-    console.log("file added")
+    uploadedFile = e.dataTransfer.files[0]
+
+    
+    if(isAccepted()){
+        console.log('file uplaoded')
+    }else{
+        console.log('file denied')
+    }
+    
 })
