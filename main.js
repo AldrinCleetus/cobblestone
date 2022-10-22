aa2a1b2771c1abe7d01e41f0f60576ab59361694
Aldrin <cleetusaldrin30@gmail.com>
2022-10-22 21:00:59 +0530

    added basic file input

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('hello')
+const shareArea = document.querySelector('.share')
+const input = shareArea.querySelector('input')
+const button = shareArea.querySelector('button')
+
+button.onclick = ()=>{
+    input.click()
+}
+
+// input.addEventListener('change',()=>{
+//     console.log('huh')
+// })
+
+shareArea.addEventListener('dragover', e=>{
+    e.preventDefault();
+    shareArea.classList.add("active")
+
+})
+
+shareArea.addEventListener('dragleave', e=>{
+    e.preventDefault();
+    shareArea.classList.remove("active")
+
+})
+
+shareArea.addEventListener('drop', e =>{
+    e.preventDefault();
+    console.log("file added")
+})
