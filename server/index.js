0ace70f2307822ba74d982b676178f41babe6044
Aldrin <cleetusaldrin30@gmail.com>
2023-02-16 06:43:28 +0530

    Fixed bug

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ import { downloadFile, sendFile } from "./routes/handleFile.js";
 const app = express()
 
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 app.use(fileUpload())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
